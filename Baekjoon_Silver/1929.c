33978a6ac2eba5c56df07363dc0c015ef075f098
Sang il Park <sangil6372@naver.com>
2022-06-30 21:48:50 +0900

    +1929

---
+++ Baekjoon_Silver/1929.c
@@ -0,0 +1,25 @@
+#pragma warning (disable:4996)
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int arr[1000001] = { 0, };
+
+int main(void) {
+	int m, n;
+	arr[1] = 1;
+
+	scanf("%d %d", &m, &n);
+
+	for (int i = 2; i <= n; i++) {
+		for (int j = 2; i * j <= n; j++) {
+			arr[i*j] = 1;
+		}
+	}
+
+	for (int i = m; i <= n; i++) {
+		if (arr[i] == 0)
+			printf("%d\n", i);
+	}
+
+	return 0;
+}
