e8b3f43e973bb9aa220500ee86205f993f7b6ab3
Sang il Park <sangil6372@naver.com>
2022-07-05 22:04:25 +0900

    +7568

--- Baekjoon_Silver/7568.c
+++ Baekjoon_Silver/7568.c
@@ -3,31 +3,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define minF(a,b) a<b?a:b
 
 
 
 int main(void) {
-	
-
-	int n;
-	int arr[50][2];
-	int cnt;
 
 
-	scanf("%d", &n);
+	int n, m;
+	int min = 80;
+	int cnt1 = 0;
+	int	cnt2 = 0;
+
+	char arr[50][50];
+
+
+	scanf("%d %d", &n, &m);
 
 	for (int i = 0; i < n; i++) {
-		scanf("%d %d", &arr[i][0], &arr[i][1]);
+		scanf("%s", &arr[i]);
 	}
-	
-	for (int i = 0; i < n; i++) {
-		cnt = 1; //자기 자신
-		for (int j = 0 ; j < n; j++) {
-			if ((arr[i][0] < arr[j][0]) && (arr[i][1] < arr[j][1]))
-				cnt++;
+
+	for (int i = 0; i < (n - 8) + 1; i++) {
+		for (int j = 0; j < (m - 8) + 1; j++) {
+			cnt1 = 0;
+			cnt2 = 0;
+
+			for (int a = i; a < i + 8; a++) {
+				for (int b = j; b < j + 8; b++) {
+
+					if ((a + b) % 2 == 0) {
+						if (arr[a][b] == 'W') {
+							cnt1++;
+						}
+						else if (arr[a][b] == 'B') {
+							cnt2++;
+						}
+					}
+					else {
+						if (arr[a][b] == 'B') {
+							cnt1++;
+						}
+						else if (arr[a][b] == 'W') {
+							cnt2++;
+						}
+					}
+
+				}
+			}
+			min = minF(min, cnt1);
+			min = minF(min, cnt2);
 		}
-		printf("%d ", cnt);
 	}
+	printf("%d", min);
 
 
 	return 0;
